05ba2d4ed0ba676698ded1c1a936ae7df4a781bc
chuqidai <daichuqi@gmail.com>
2016-01-20 16:22:22 -0800

    [add]favicon

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var path = require('path');
 var server = express();
 var PORT = process.env.PORT || 3000;
+var favicon = require('serve-favicon');
 
-
+server.use(favicon(path.join(__dirname, '/public', 'favicon.ico')));
 server.use(express.static(__dirname + '/public'));
+
 
 server.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'));
